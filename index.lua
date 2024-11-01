8d43b0520284d4fe4ab8f372a0159d650b953458
Doo <tammai488@gmail.com>
2024-11-01 10:16:22 +0700

    first commit

---
+++ index.lua
@@ -0,0 +1,17 @@
+local Identifier = {
+    'fivemid:123456',
+        'steam:456465',
+        'Identifier:45646556'
+
+}
+
+for i, v in ipairs(Identifier) do
+    print(i, v)
+  end
+
+local a = 5 
+
+local b = 10
+
+print(a+b)
+  
